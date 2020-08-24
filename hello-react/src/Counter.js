77cc4ee4e853a25072509dd9fa033daafc55b3e9
ashzero <lyricalzy@gmail.com>
2020-08-24 15:03:36 +0900

    react tutorial

---
+++ hello-react/src/Counter.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+const Problematic = () => {
+    throw (new Error('버그가 나타났다!'));
+    return(
+        <div>
+
+        </div>
+    );
+};
+
+class Counter extends Component {
+    state = {
+        number: 0
+    }
+
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount (deprecated)');
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('shouldComponentUpdate');
+        if (nextState.number % 5 === 0) return false;
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('componentWillUpdate');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate');
+    }
+
+    handleIncrease = () => {
+        const { number } = this.state;
+        this.setState({
+           number: number + 1
+        });
+    }
+
+    handleDecrease = () => {
+        const { number } = this.state;
+        this.setState({
+           number: number - 1
+        });
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({
+            error: true
+        });
+    }
+
+    render() {
+        console.log('render');
+
+        if (this.state.error) return (<h1>에러발생!</h1>);
+        
+        return (
+            <div>
+                <h1>카운터</h1>
+                <div>값: {this.state.number}</div>
+                { this.state.number === 4 && <Problematic />}
+                <button onClick={this.handleIncrease}>+</button>
+                <button onClick={this.handleDecrease}>-</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
